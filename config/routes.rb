910607aa6cd68930bc5a737087264c091aa75723
Megumi-0712 <toruansya@yahoo.co.jp>
2020-05-28 01:44:40 +0900

    メニューのリンク修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: "top#index"
-  get 'about' => 'session#index'
+  get "home/about" => "top#about"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
